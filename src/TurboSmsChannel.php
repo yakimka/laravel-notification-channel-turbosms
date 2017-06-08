dc50cd709645481b8b6bb2d08e8c8b74742296ae
yakimka <ss.yakim@gmail.com>
2017-06-09 00:26:32 +0300

    StyleCI

--- src/TurboSmsChannel.php
+++ src/TurboSmsChannel.php
@@ -1,42 +1,50 @@
 <?php
+
 namespace NotificationChannels\TurboSms;
+
 use Illuminate\Notifications\Notification;
 use NotificationChannels\TurboSms\Exceptions\CouldNotSendNotification;
 
-class TurboSmsChannel {
-	/** @var \NotificationChannels\TurboSms\TurboSmsApi */
-	protected $turbosms;
-	public function __construct(TurboSmsApi $turbosms) {
-		$this->turbosms = $turbosms;
-	}
-	/**
-	 * Send the given notification.
-	 *
-	 * @param mixed $notifiable
-	 * @param \Illuminate\Notifications\Notification $notification
-	 *
-	 * @throws \NotificationChannels\TurboSms\Exceptions\CouldNotSendNotification
-	 */
+class TurboSmsChannel
+{
+    /** @var \NotificationChannels\TurboSms\TurboSmsApi */
+    protected $turbosms;
 
-	public function send($notifiable, Notification $notification) {
-		$to = $notifiable->routeNotificationFor('turbosms');
-		if (empty($to)) {
-			throw CouldNotSendNotification::missingRecipient();
-		}
-		$message = $notification->toTurbosms($notifiable);
-		if (is_string($message)) {
-			$message = new TurboSmsMessage($message);
-		}
-		$this->sendMessage($to, $message);
-	}
-	protected function sendMessage($recipient, TurboSmsMessage $message) {
-		if (mb_strlen($message->content) > 800) {
-			throw CouldNotSendNotification::contentLengthLimitExceeded();
-		}
-		$params = [
-			'phone' => $recipient,
-			'text' => $message->content,
-		];
-		$this->turbosms->send($params);
-	}
+    public function __construct(TurboSmsApi $turbosms)
+    {
+        $this->turbosms = $turbosms;
+    }
+
+    /**
+     * Send the given notification.
+     *
+     * @param mixed $notifiable
+     * @param \Illuminate\Notifications\Notification $notification
+     *
+     * @throws \NotificationChannels\TurboSms\Exceptions\CouldNotSendNotification
+     */
+    public function send($notifiable, Notification $notification)
+    {
+        $to = $notifiable->routeNotificationFor('turbosms');
+        if (empty($to)) {
+            throw CouldNotSendNotification::missingRecipient();
+        }
+        $message = $notification->toTurbosms($notifiable);
+        if (is_string($message)) {
+            $message = new TurboSmsMessage($message);
+        }
+        $this->sendMessage($to, $message);
+    }
+
+    protected function sendMessage($recipient, TurboSmsMessage $message)
+    {
+        if (mb_strlen($message->content) > 800) {
+            throw CouldNotSendNotification::contentLengthLimitExceeded();
+        }
+        $params = [
+            'phone' => $recipient,
+            'text' => $message->content,
+        ];
+        $this->turbosms->send($params);
+    }
 }
--- src/TurboSmsServiceProvider.php
+++ src/TurboSmsServiceProvider.php
@@ -1,21 +1,28 @@
 <?php
+
 namespace NotificationChannels\TurboSms;
+
 use Illuminate\Support\ServiceProvider;
 
-class TurboSmsServiceProvider extends ServiceProvider {
-	/**
-	 * Bootstrap the application services.
-	 */
-	public function boot() {
-		//
-	}
-	/**
-	 * Register the application services.
-	 */
-	public function register() {
-		$this->app->singleton(TurboSmsApi::class, function () {
-			$config = config('services.turbosms');
-			return new TurboSmsApi($config['login'], $config['secret'], $config['sender'], $config['url']);
-		});
-	}
+class TurboSmsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->app->singleton(TurboSmsApi::class, function () {
+            $config = config('services.turbosms');
+
+            return new TurboSmsApi($config['login'], $config['secret'], $config['sender'], $config['url']);
+        });
+    }
 }

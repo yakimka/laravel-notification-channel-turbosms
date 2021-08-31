70a54ef726dccc26e66fee9b75e10530e39f1387
yakimka <ss.yakim@gmail.com>
2021-08-31 19:45:04 +0300

    Add tests

--- tests/TurboSmsMessageTest.php
+++ tests/TurboSmsMessageTest.php
@@ -3,34 +3,35 @@
 namespace NotificationChannels\TurboSms\Test;
 
 use NotificationChannels\TurboSms\TurboSmsMessage;
+use PHPUnit\Framework\TestCase;
 
-class TurboSmsMessageTest extends \PHPUnit_Framework_TestCase
+class TurboSmsMessageTest extends TestCase
 {
-    /** @test */
-    public function it_can_accept_a_content_when_constructing_a_message()
+    public function test_it_can_accept_a_content_when_constructing_a_message(): void
     {
         $message = new TurboSmsMessage('hello');
+
         $this->assertEquals('hello', $message->content);
     }
 
-    /** @test */
-    public function it_can_accept_a_content_when_creating_a_message()
+    public function test_it_can_accept_a_content_when_creating_a_message(): void
     {
         $message = TurboSmsMessage::create('hello');
+
         $this->assertEquals('hello', $message->content);
     }
 
-    /** @test */
-    public function it_can_set_the_content()
+    public function test_it_can_set_the_content(): void
     {
         $message = (new TurboSmsMessage())->content('hello');
+
         $this->assertEquals('hello', $message->content);
     }
 
-    /** @test */
-    public function it_can_set_the_from()
+    public function test_it_can_set_the_from(): void
     {
         $message = (new TurboSmsMessage())->from('John_Doe');
+        
         $this->assertEquals('John_Doe', $message->from);
     }
 }
